76407b4bae2bc643f69bcdf055182110fccd8da7
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-15 11:36:29 +0200

    A Student osztály két konstuktora

--- StudentProject/Models/Student.cs
+++ StudentProject/Models/Student.cs
@@ -12,5 +12,25 @@ namespace StudentProject.Models
         public int SchoolYear { get; set; }
         public SchoolClassType SchoolClass { get; set; }
         public string EducationLevel { get; set; }
+
+        public Student(string firstName, string lastName, DateTime birthsDay, int schoolYear, SchoolClassType schoolClass, string educationLevel)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthsDay = birthsDay;
+            SchoolYear = schoolYear;
+            SchoolClass = schoolClass;
+            EducationLevel = educationLevel;
+        }
+
+        public Student()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            BirthsDay = new DateTime();
+            SchoolYear = 9;
+            SchoolClass = SchoolClassType.ClassA;
+            EducationLevel = string.Empty;
+        }
     }
 }
