d3f06ff25621f0c4ed4d2e6013d0436884371bd2
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-15 11:35:15 +0200

    Student property-k (tulajdonságok)

--- StudentProject/Models/Student.cs
+++ StudentProject/Models/Student.cs
@@ -1,8 +1,16 @@
-﻿namespace StudentProject.Models
+﻿using System;
+
+namespace StudentProject.Models
 {
     public enum SchoolClassType { ClassA, ClassB, ClassC}
 
     public class Student
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthsDay { get; set; }
+        public int SchoolYear { get; set; }
+        public SchoolClassType SchoolClass { get; set; }
+        public string EducationLevel { get; set; }
     }
 }
