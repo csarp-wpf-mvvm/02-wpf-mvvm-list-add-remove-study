ebd261662eadacd8af58e13d8b4416b806ee32fe
gycsaba <gyuris.csaba@vasvari.org>
2023-09-16 13:32:42 +0200

    A ViewModel egy Student-et tartlamaz amelyet a View réteg jelenít meg.

--- StudentProject/ViewModels/StudentViewModel.cs
+++ StudentProject/ViewModels/StudentViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using StudentProject.Models;
 
 namespace StudentProject.ViewModels
 {
     public partial class StudentViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private Student _student;
     }
 }
